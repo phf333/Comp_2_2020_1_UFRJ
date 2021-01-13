2603e7d623283b2af57d056e5c1d02529857a59a
Paulo Henrique <phf333@gmail.com>
2021-01-13 00:01:00 -0300

    lab4

---
+++ [Comp2_2020_1] LAB4/src/Figurinha.java
@@ -0,0 +1,23 @@
+public class Figurinha {
+
+    private int posicao;
+
+    public Figurinha(int posicao) {
+        this.posicao=posicao;
+
+    }
+
+    /**
+     * @return A posição que esta figurinha deve ocupar no álbum
+     */
+    public int getPosicao() {
+        return this.posicao;
+    }
+
+    @Override
+    public String toString() {
+        return "Figurinha{" +
+                "posicao=" + posicao +
+                '}';
+    }
+}
---
+++ [Comp2_2020_1] LAB4/src/InterfaceTexto.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class InterfaceTexto {
+
+
+    /**
+     * Main apenas para testes muito rápidos. Melhor seria escrever unit tests de verdade.
+     * Na prática, o seu "main()" abriria uma interface de texto de verdade,
+     * apresentando um menu, etc.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Album album = new Album(200, 3);
+
+        Pacotinho p1 = new Pacotinho(album, new int[] {1, 6, 18});
+        Pacotinho p5 = new Pacotinho(album, new int[] {1, 6, 18});
+
+        ArrayList<Figurinha> teste = new ArrayList<>();
+
+        Pacotinho p7 = new Pacotinho(album);
+        album.receberNovoPacotinho(p5);
+        album.receberNovoPacotinho(p1);
+
+        //System.out.println(p7);
+        System.out.println(album);
+        System.out.println(album.getQuantFigurinhasColadas());
+        System.out.println(album.getQuantFigurinhasRepetidas());
+
+
+//        System.out.println("Possui figurinha 1? " + album.possuiFigurinhaColada(1));
+//        System.out.println("Possui figurinha 6? " + album.possuiFigurinhaColada(6));
+//        System.out.println("Possui figurinha 18? " + album.possuiFigurinhaColada(18));
+//        System.out.println("Possui figurinha 60? " + album.possuiFigurinhaColada(60));
+//
+//        System.out.println("Quant figurinhas coladas? " + album.getQuantFigurinhasColadas());
+//
+//        Pacotinho p2 = new Pacotinho(album, new int[] {3, 6, 44});
+//
+//        System.out.println("Tem figurinha repetida 6? " + album.possuiFigurinhaRepetida(6));
+//        System.out.println("Quant figurinhas coladas? " + album.getQuantFigurinhasColadas());
+
+
+    }
+
+}
+
